a753295cecb877ba14ef6fa645903667826610d2
Asish.sundaray <asish.sundaray72@gmail.com>
2023-02-15 22:45:45 +0530

    remove accesskey of aws

--- Provider.tf
+++ Provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAS2OLAZ4DQZNFE22S"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
